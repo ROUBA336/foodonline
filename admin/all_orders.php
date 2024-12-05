e5e3ab7f352c4a49410d159971e5e9098b3804b0
ROUBA336 <rabouchayach@gmail.com>
2024-12-05 15:34:40 +0200

    Initial commit

---
+++ admin/all_orders.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include("../connection/connect.php");
+error_reporting(0);
+session_start();
+?>
+  
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <title>All Orders</title>
+    <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+    <link href="css/helper.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body class="fix-header fix-sidebar">
+
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+        </svg>
+    </div>
+
+    <div id="main-wrapper">
+
+        <div class="header">
+            <nav class="navbar top-navbar navbar-expand-md navbar-light">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="dashboard.php">
+                        <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
+                    </a>
+                </div>
+                <div class="navbar-collapse">
+                    <ul class="navbar-nav mr-auto mt-md-0">
+                    </ul>
+
+                    <ul class="navbar-nav my-lg-0">
+                        <li class="nav-item dropdown">
+                            <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
+                                <ul>
+                                    <li>
+                                        <div class="drop-title">Notifications</div>
+                                    </li>
+                                    <li>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <div class="dropdown-menu dropdown-menu-right animated zoomIn">
+                                <li class="nav-item">
+                                    <a class="nav-link text-muted" href="logout.php">
+                                        <button class="btn btn-danger"><i class="fa fa-sign-out-alt"></i> Log Out</button>
+                                    </a>
+                                </li>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+
+        <div class="left-sidebar">
+            <div class="scroll-sidebar">
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-devider"></li>
+                        <li> <a href="all_users.php"> <span><i class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                        <li> <a class="has-arrow" href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="all_menu.php">All Menues</a></li>
+                                <li><a href="add_menu.php">Add Menu</a></li>
+                            </ul>
+                        </li>
+                        <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span></a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        <div class="page-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="col-lg-12">
+                            <div class="card card-outline-primary">
+                                <div class="card-header">
+                                    <h4 class="m-b-0 text-white">All Orders</h4>
+                                </div>
+
+                                <div class="table-responsive m-t-40">
+                                    <table id="myTable" class="table table-bordered table-striped">
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th>User</th>
+                                                <th>Title</th>
+                                                <th>Quantity</th>
+                                                <th>Price</th>
+                                                <th>Address</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $sql = "SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id ";
+                                            $query = mysqli_query($db, $sql);
+
+                                            if (!mysqli_num_rows($query) > 0) {
+                                                echo '<td colspan="6"><center>No Orders</center></td>';
+                                            } else {
+                                                while ($rows = mysqli_fetch_array($query)) {
+                                                    echo '<tr>
+                                                        <td>' . $rows['username'] . '</td>
+                                                        <td>' . $rows['title'] . '</td>
+                                                        <td>' . $rows['quantity'] . '</td>
+                                                        <td>$' . $rows['price'] . '</td>
+                                                        <td>' . $rows['address'] . '</td>
+                                                      </tr>';
+                                                }
+                                            }
+                                            ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <?php include "include/footer.php" ?>
+    </div>
+
+    <script src="js/lib/jquery/jquery.min.js"></script>
+   
+   
+    <script src="js/custom.min.js"></script>
+
+  
+</body>
+</html>
